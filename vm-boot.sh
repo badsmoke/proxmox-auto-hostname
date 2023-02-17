a4efaac341c00522ffb476a20c7083ca11ce0ce2
badsmoke <github@badcloud.eu>
2023-02-17 09:10:08 +0100

    add "null" name check, add more quotes

--- vm-boot.sh
+++ vm-boot.sh
@@ -11,29 +11,32 @@ get_hostname(){
 
   #get ticket
   DATA=`curl -s4 -k -d "username=$PROX_USERNAME&password=$PROX_PASSWORD" $HOST/api2/json/access/ticket`
-  TICKET=$(echo $DATA | jq -r .data.ticket )
-  CSRF=$(echo $DATA | jq -r .data.CSRFPreventionToken)
+  TICKET=$(echo "${DATA}" | jq -r .data.ticket )
+  CSRF=$(echo "${DATA}" | jq -r .data.CSRFPreventionToken)
 
   #get all vms
   DATA=$(curl -s4k -b "PVEAuthCookie=$TICKET" $HOST/api2/json/cluster/resources | jq -S -r '(.data[] |select(.type=="qemu")) ')
-  NODE=($(echo $DATA | jq -r .node))
-  VMID=($(echo $DATA | jq -r .vmid))
-  NAME=($(echo $DATA | jq -r .name | sed s/'VM.[0-9]*'/unnamed/g))
+  NODE=($(echo "${DATA}" | jq -r .node))
+  VMID=($(echo "${DATA}" | jq -r .vmid))
+  NAME=($(echo "${DATA}" | jq -r .name | sed s/'VM.[0-9]*'/unnamed/g))
   echo "own mac"$MAC_ADDRESS
   #get interface mac address
   for ((i = 0 ; i < ${#VMID[@]} ; i++)); do
     DATA=$(curl -s4k -b "PVEAuthCookie=$TICKET" $HOST/api2/json/nodes/${NODE[$i]}/qemu/${VMID[$i]}/config)
-    MAC=$(echo $DATA | jq .data.net0 | cut -d "=" -f2 | cut -d "," -f1)
+    MAC=$(echo "${DATA}" | jq .data.net0 | cut -d "=" -f2 | cut -d "," -f1)
     #compare mac adress
     if [[ "${MAC_ADDRESS[@]}" =~ "${MAC,,}" ]]; then
         echo $MAC
-        echo ${NAME[$i]}
+        echo "${NAME[$i]}"
         #set hostame
-        if [[ -n ${NAME[$i]} ]];then
-          if [[ ! $(cat /etc/hostname) = ${NAME[$i]} ]];then
-                hostnamectl set-hostname ${NAME[$i]}
-                HOSTNAME=${NAME[$i]}
-                reboot
+        if [[ ! "${NAME[$i]}" == "null" ]];then 
+          if [[ -n "${NAME[$i]}" ]];then
+            if [[ ! $(cat /etc/hostname) = "${NAME[$i]}" ]];then
+                  hostnamectl set-hostname "${NAME[$i]}"
+                  HOSTNAME="${NAME[$i]}"
+                  recreate_promtail
+                  reboot
+            fi
           fi
         fi
         break
